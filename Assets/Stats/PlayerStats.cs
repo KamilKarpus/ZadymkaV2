ba05d4d02f4b976d2e5487b94892f5bc09b9e5f2
Cholewa98 <cholewka.michal@gmail.com>
2020-04-16 20:53:14 +0200

    add healthbar

--- Assets/Stats/PlayerStats.cs
+++ Assets/Stats/PlayerStats.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : CharacterStats
 {
     private Animator animator;
+    public Text nameText;
+    public Text lvlText;
+
+
     private void Start()
     {
-        animator = GetComponent<Animator>();  
+        animator = GetComponent<Animator>();
+        level = 99;
+        name = "Pszemek";
     }
     protected override void Die()
     {
         base.Die();
         animator.SetInteger("condition", 10);
         isDeath = true;
+    }
+
+    public void Update()
+    {
+        lvlText.text = "Lvl: " + level.ToString();
+        nameText.text = name;
     }
 }
